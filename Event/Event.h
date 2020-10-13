5a502eb1a2b3bff8a48be2e47256a187ec81bb4e
ming <gamesoftware2010@gmail.com>
2020-10-13 19:57:17 +0800

    改为不定参数

--- Event/Event.h
+++ Event/Event.h
@@ -3,34 +3,34 @@
 #include <map>
 using namespace std;
 
-template<typename _type, typename _param>
+template<typename _type, typename... _param>
 class Event
 {
 public:
 	Event(){}
 	~Event(){}
 
-	//ÓÒÖµ
-	void emit(_type&& name, const _param&& tp) {
+	//å³å€¼
+	void emit(_type&& name, const _param&& ...tp) {
 		auto _list = m_mapEvent.equal_range(name);
 		for (auto it = _list.first; it != _list.second; ++it) {
-			it->second(tp);
+			it->second(tp...);
 		}
 	}
 
-	//×óÖµ
-	void emit(_type&& name, const _param& tp) {
+	//å·¦å€¼
+	void emit(_type&& name, const _param& ...tp) {
 		auto _list = m_mapEvent.equal_range(name);
 		for (auto it = _list.first; it != _list.second; ++it) {
-			it->second(tp);
+			it->second(tp...);
 		}
 	}
 
-	void on(_type&& name, function<void(const _param&)>&& callback) {
+	void on(_type&& name, function<void(const _param&...)>&& callback) {
 		m_mapEvent.insert(make_pair(name, callback));
 	}
 
 private:
-	multimap<_type, function<void(const _param&)>> m_mapEvent;
+	multimap<_type, function<void(const _param&...)>> m_mapEvent;
 };
 
